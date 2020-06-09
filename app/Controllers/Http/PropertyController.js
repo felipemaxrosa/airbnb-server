6a8bba9b351a70d6a9787dd2d405de676a8abc32
FelipeMaxRosa <felipemax.suporteti@gmail.com>
2020-06-08 22:19:51 -0300

    implementation store method of the PropertyController

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -20,6 +20,7 @@ class PropertyController {
    */
   async index ({ request, response, view }) {
     const { latitude, longitude } = request.all();
+
     const properties = Property.query()
       .nearBy(latitude, longitude, 10)
       .fetch();
@@ -37,7 +38,19 @@ class PropertyController {
    * @param {View} ctx.view
    */
 
-  async store ({ request, response }) {
+  async store ({ auth, request, response }) {
+    const { id } = auth.user
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+  
+    const property = await Property.create({ ...data, user_id: id })
+  
+    return property
   }
 
   /**
